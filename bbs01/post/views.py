44423fb5cb923eb4d3bfc3528f28f84baac7920a
nie1511 <1511271730@qq.com>
2018-06-20 11:07:29 +0800

    post功能上传

---
+++ bbs01/post/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+
+from math import ceil
+
+from post.models import Post
+
+
+# Create your views here.
+
+def create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        post = Post.objects.create(title=title, content=content)
+        return redirect('/post/read/?post_id=%d' % post.id)
+    return render(request, 'create.html')
+
+
+def edit(request):
+    if request.method == 'POST':
+        post_id = int(request.POST.get('post_id'))
+        post = Post.objects.get(id=post_id)
+
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('/post/read/?post_id=%d' % post.id)
+    else:
+        post_id = int(request.POST.get('post_id'))
+        post = Post.objects.get(id=post_id)
+        return render(request, 'edit.html', {'post': post})
+
+
+def read(request):
+    post_id = int(request.POST.get('post_id'))
+    post = Post.objects.get(id=post_id)
+    return render(request, 'read.html', {'post': post})
+
+
+def post_list(request):
+    page = int(request.GET.get('page', 1))
+    per_page = 5
+    pages = ceil(Post.objects.count() / per_page)
+
+    start = (page - 1) * per_page
+    end = start + 5
+
+    posts = Post.objects.all().order_by('-id')[start:end]
+    return render(request, 'post_list.html', {'posts':posts,'pages':range(pages)})
+
+
+def search(request):
+    keyword =request.POST.get('keyword')
+    posts =Post.objects.filter(content__contains=keyword)
+    return render(request, 'search.html', {'posts':posts})
